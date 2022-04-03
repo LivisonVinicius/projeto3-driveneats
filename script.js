123310da3718a58936cc99fef5e9108c11bbb2eb
LivisonVinicius <livisongc@gmail.com>
2022-04-03 17:08:25 -0300

    Requisitos + Bônus

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function liberarBotao() {
         botaoDesativo.classList.add("escondido")
     }
 }
-
 function seleciona(opcao,tipo){
     const opcaoSelecionada=document.querySelector(`.${tipo} .Selecionada`);
     if (opcaoSelecionada !== null) {
@@ -19,18 +18,51 @@ function seleciona(opcao,tipo){
 function fechaConta(){
     let comida = document.querySelector(".Comida .Selecionada");
     let drink = document.querySelector(".Drink .Selecionada");
-    let desert = document.querySelector(".Desert .Selecionada");
+    let dessert = document.querySelector(".Dessert .Selecionada");
     const nomeComida=comida.querySelector("h4").innerHTML;
     const nomeDrink=drink.querySelector("h4").innerHTML;
-    const nomeDesert=desert.querySelector("h4").innerHTML;
+    const nomeDessert=dessert.querySelector("h4").innerHTML;
+    const nome= prompt("Insira o seu nome:")
+    const endereço=prompt("Insira o endereço da entrega:");
     comida=comida.querySelector("h6").innerHTML.slice(2).replace(",", ".");
     drink=drink.querySelector("h6").innerHTML.slice(2).replace(",", ".");
-    desert=desert.querySelector("h6").innerHTML.slice(2).replace(",", ".");
-    const valorFinal= (Number(comida)+Number(drink)+Number(desert)).toFixed(2);
+    dessert=dessert.querySelector("h6").innerHTML.slice(2).replace(",", ".");
+    const valorFinal= (Number(comida)+Number(drink)+Number(dessert)).toFixed(2);
     window.open("https://wa.me/5521965778101?text="+encodeURIComponent(`Olá, gostaria de fazer o pedido:
     - Prato: ${nomeComida}
     - Bebida: ${nomeDrink}
-    - Sobremesa: ${nomeDesert}
-    Total: R$ ${valorFinal}`));
+    - Sobremesa: ${nomeDessert}
+    Total: R$ ${valorFinal}
+    
+    Nome: ${nome}
+    Endereço: ${endereço}`));
+}
+
+function confirmacao() {
+    const confirmar=document.querySelector(".confirmacao")
+    confirmar.classList.remove("escondido")
+    let comida = document.querySelector(".Comida .Selecionada");
+    let drink = document.querySelector(".Drink .Selecionada");
+    let dessert = document.querySelector(".Dessert .Selecionada");
+    const nomeComida=comida.querySelector("h4").innerHTML;
+    const nomeDrink=drink.querySelector("h4").innerHTML;
+    const nomeDessert=dessert.querySelector("h4").innerHTML;
+    comida=comida.querySelector("h6").innerHTML.slice(2).replace(",", ".");
+    drink=drink.querySelector("h6").innerHTML.slice(2).replace(",", ".");
+    dessert=dessert.querySelector("h6").innerHTML.slice(2).replace(",", ".");
+    const valorFinal= (Number(comida)+Number(drink)+Number(dessert)).toFixed(2);
+    document.querySelector(".comida h4").innerHTML=nomeComida
+    document.querySelector(".bebida h4").innerHTML=nomeDrink
+    document.querySelector(".sobremesa h4").innerHTML=nomeDessert
+    document.querySelector(".valorFinal h5").innerHTML=valorFinal
+    document.querySelector(".comida h5").innerHTML=comida
+    document.querySelector(".bebida h5").innerHTML=drink
+    document.querySelector(".sobremesa h5").innerHTML=dessert
+
+    
+}
+function cancelar(){
+    const cancelar=document.querySelector(".confirmacao")
+    cancelar.classList.add("escondido")
 }
 
